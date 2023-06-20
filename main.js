f423bbb2dc9f9b0e927a642d928901ec354ec4f2
Dean <dean@dagshub.com>
2023-06-20 11:19:09 +0300

    Make the app full screen, always on top, and pass through

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, screen } = require('electron')
 
 function createWindow() {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
   win = new BrowserWindow({
-    width: 1512,
-    height: 982,
+    width: width,
+    height: height,
     transparent:true,
+    frame: false,
+    alwaysOnTop: true,
     webPreferences: {
       nodeIntegration: true
     }
   })
+  win.setIgnoreMouseEvents(true, { forward: true }); // Allow clicks to pass through
   win.maximize();
   win.loadFile('googly.html') // Replace 'your-app.html' with the actual file name of your HTML file
 
